34f116153b9beba91002dad43260d50319a89b3b
nsaoirse <nicolette.s.lewis@outlook.com>
2024-11-17 10:14:18 -0800

    updated hdro examples for clarity in case setting definition-highlighting json input file format

--- hdro-0004/coupledAnalysisSettings.py
+++ hdro-0004/coupledAnalysisSettings.py
@@ -1,101 +1,4 @@
 
-
-oneWay=0 # if this is 1, then the displacements calculated by OpenSees are not transferred to OpenFOAM
-#also..  # if this is 2, then the forces calculated by OpenFOAM are not transferred to OpenSees
-
-
-############      NOTE : If FOAMySees if run 'as a part of HydroUQ', most if not all of these settings
-############		will likely be overwritten! 
-############
-############
-############
-############
-############
-############  Input the settings for your coupled analysis below 
-
-numOpenSeesStepsPerCouplingTimestep=1
-numOpenFOAMStepsPerCouplingTimestep=1
-
-
-
-#### Timing Settings #####
-############ BOTH OpenFOAM and OpenSees
-SolutionDT=1e-4 # this is the coupling timestep length
-runPrelim='yes' # run the preliminary analysis defined (maybe remove this???)
-startOFSimAt=0.0
-endTime=1
-
-
-
-###########################################################################################################
-###########################################################################################################
-###########################################################################################################
-# openSees settings
-ApplyGravity='yes'
-g=[0,0,-9.81]
-
-###########################################################################################################
-###########################################################################################################
-###########################################################################################################
-OpenSeesconvergenceTol=1e-8
-#'EnergyIncr', Tol, maxNumIter
-Test=["NormUnbalance",1e-8,1000]
-Integration=["Newton",0.5,0.25]
-Algorithm="KrylovNewton"
-OpenSeesSystem='BandGen'
-OpenSeesConstraints='Transformation'
-Numberer='RCM'
-OSndm=3
-OSndf=6
-Analysis=["VariableTransient","-numSubLevels",2,"-numSubSteps",10]
-
-###########################################################################################################
-###########################################################################################################
-###########################################################################################################
-
-
-# OpenFOAM...
-AdjustTimeStep='no'
-SimDuration=endTime
-Turbulence="No"
-interfaceSurface="interface.stl"
-DecompositionMethod="scotch"
-
-
-###########################################################################################################
-###########################################################################################################
-###########################################################################################################
-
-###########################################################################################################
-###########################################################################################################
-###########################################################################################################
-#### Coupling Settings #####
-CouplingScheme="Implicit" # "Explicit"
-timeWindowsReused=3		# number of past time windows used to approximate secant behavior
-iterationsReused=5		# number of iterations used to accelerate coupling data
-couplingConvergenceTol=5e-3     # coupling data relative residual convergence value
-initialRelaxationFactor=0.1     # initial relaxation factor used in dynamic relaxation scheme
-
-couplingDataAccelerationMethod="IQN-ILS" #Constant Aitken IQN-IMVJ Broyden
-
-maximumCouplingIterations=100 #set this to a high value
-
-mapType='nearest-neighbor' #'rbf-thin-plate-splines'# either or - nearest-neighbor is faster, rbf is more robust...
-
-###########################################################################################################
-###########################################################################################################
-###########################################################################################################
-#### OUTPUT SETTINGS ######
-#OpenFOAM Write Frequency
-writeDT=0.1 # seconds
-
-#OpenSeesPy Write Frequency
-SeesVTKOUTRate=0.1 # seconds
-
-# This is to output data during the coupling iterations from preCICE library data transfers. Could help with debugging, but generally is best to leave as "No"
-outputDataFromCouplingIterations="No"
-couplingIterationOutputDataFrequency="1000"
-
 ###########################################################################################################
 ###########################################################################################################
 # /*--------------------------------*- C++ -*----------------------------------*\
@@ -114,11 +17,17 @@ couplingIterationOutputDataFrequency="1000"
 # Thank you to UW HYAK and to the support staff of the UW HPC resources for their maintenance of the supercomputer cluster and for offering a stable platform for HPC development 
 # and computation, as well as for all of the great support over the last few years.  
 
-
+############      NOTE : If FOAMySees if run 'as a part of HydroUQ', most if not all of these settings
+############		will likely be overwritten! 
+############    
+############
+############        CHECK THE FILE caseSetup.sh to see if the variable FYS_isPartOfHydro="Yes"
+############        if so, the settings below which are "Hydro UQ controlled" must be defined in src/input.json
+############
+############  Input the settings for your coupled analysis below 
 
 ###########################################################################################################
 ###########################################################################################################
-
 
 # Set fixity options
 ########################### only use if you want to
@@ -135,3 +44,91 @@ fixXatFixity=[1,1,1,1,1,1] # this is the fixity BC which will be applied at the 
 fixYatFixity=[1,1,1,1,1,1] # this is the fixity BC which will be applied at the coordinates within 'fixYat' list
 fixZatFixity=[1,1,1,1,1,1] # this is the fixity BC which will be applied at the coordinates within 'fixZat' list
 
+oneWay=0 # if this is 1, then the displacements calculated by OpenSees are not transferred to OpenFOAM
+#also..  # if this is 2, then the forces calculated by OpenFOAM are not transferred to OpenSees
+
+numOpenSeesStepsPerCouplingTimestep=1
+numOpenFOAMStepsPerCouplingTimestep=1
+
+#### Timing Settings #####
+############ BOTH OpenFOAM and OpenSees
+startOFSimAt=0.0
+couplingStartTime=0
+SolutionDT="Hydro UQ controlled" # this is the coupling timestep length
+runPrelim="Hydro UQ controlled" # run the preliminary analysis defined (maybe remove this???)
+endTime="Hydro UQ controlled"
+runSnappyHexMesh="Hydro UQ controlled" 
+
+###########################################################################################################
+###########################################################################################################
+###########################################################################################################
+# openSees settings
+ApplyGravity='yes'
+g=[0,0,-9.81]
+
+###########################################################################################################
+###########################################################################################################
+###########################################################################################################
+OpenSeesconvergenceTol=1e-8
+#'EnergyIncr', Tol, maxNumIter
+Test=["NormUnbalance",OpenSeesconvergenceTol,1000]
+Integration=["Newmark",0.5,0.25]
+Algorithm="KrylovNewton"
+OpenSeesSystem='BandGen'
+OpenSeesConstraints='Transformation'
+Numberer='RCM'
+OSndm=3
+OSndf=6
+Analysis=["VariableTransient","-numSubLevels",2,"-numSubSteps",10]
+
+###########################################################################################################
+###########################################################################################################
+###########################################################################################################
+
+
+# OpenFOAM...
+AdjustTimeStep="Hydro UQ controlled" 
+SimDuration="Hydro UQ controlled" 
+Turbulence="Hydro UQ controlled" 
+interfaceSurface="Hydro UQ controlled" 
+DecompositionMethod="Hydro UQ controlled" 
+
+
+###########################################################################################################
+###########################################################################################################
+###########################################################################################################
+
+###########################################################################################################
+###########################################################################################################
+###########################################################################################################
+#### Coupling Settings #####
+# CouplingScheme="Implicit" # "Explicit"
+CouplingScheme="Hydro UQ controlled" 
+
+# timeWindowsReused=3		# number of past time windows used to approximate secant behavior
+# iterationsReused=5		# number of iterations used to accelerate coupling data
+# couplingConvergenceTol=5e-3     # coupling data relative residual convergence value
+# initialRelaxationFactor=0.1     # initial relaxation factor used in dynamic relaxation scheme
+# couplingDataAccelerationMethod="IQN-ILS" #Constant Aitken IQN-IMVJ Broyden
+# maximumCouplingIterations=100 #set this to a high value
+# mapType='nearest-neighbor' #'rbf-thin-plate-splines'# either or - nearest-neighbor is faster, rbf is more robust...
+
+timeWindowsReused="Hydro UQ controlled" 
+iterationsReused="Hydro UQ controlled" 
+couplingConvergenceTol="Hydro UQ controlled" 
+initialRelaxationFactor="Hydro UQ controlled" 
+couplingDataAccelerationMethod="Hydro UQ controlled" 
+maximumCouplingIterations="Hydro UQ controlled" 
+mapType="Hydro UQ controlled" 
+
+###########################################################################################################
+#### OUTPUT SETTINGS ######
+#OpenFOAM Write Frequency
+writeDT="Hydro UQ controlled"  # seconds
+
+#OpenSeesPy Write Frequency
+SeesVTKOUTRate="Hydro UQ controlled"  # seconds
+
+# This is to output data during the coupling iterations from preCICE library data transfers. Could help with debugging, but generally is best to leave as "No"
+outputDataFromCouplingIterations="No"
+couplingIterationOutputDataFrequency="1000"
