575dee6670c92e452408f63c9e36ddc087214080
nsaoirse <nicolette.s.lewis@outlook.com>
2024-11-17 08:29:21 -0800

    updated cantileverBeam/noGroundMotion case

--- CantileverBeam/concurrentGroundMotion/RunCase/config_helpers/copyUserInputsToCase.py
+++
@@ -1,5 +0,0 @@
-from subprocess import Popen
-def copyUserInputsToCase(fromHere='../userInputs'):
-	Popen("cp "+fromHere+"/* .",shell=True)
-	print('copied input case files to RunCase directory')
-
--- CantileverBeam/noGroundMotion/RunCase/fromUserDefaults/userLoadRoutines.py
+++
@@ -1,6 +0,0 @@
-import openseespy.opensees as ops
-def applyGM(startTime):
-	pass
-	
-def removeGM():
-	pass
